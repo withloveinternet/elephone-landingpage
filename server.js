10ff105c04b92212d8ef72325beb98f544b87657
Bob Singor <bobsingor@Bobs-MacBook-Pro.local>
2016-06-15 18:28:58 +0300

    Base for simple elephone landing page

---
+++ server.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+var express = require("express"),
+    app = express(),
+    bodyParser = require('body-parser'),
+    errorHandler = require('errorhandler'),
+    methodOverride = require('method-override'),
+    port = parseInt(process.env.PORT, 10) || 1337,
+    publicDir = process.argv[2] || __dirname + '/public',
+    path = require('path');
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(publicDir, "/index.html"));
+});
+
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static(publicDir));
+app.use(errorHandler({
+  dumpExceptions: true,
+  showStack: true
+}));
+
+console.log("Simple static server showing %s listening at http://%s:%s", publicDir, port);
+app.listen(port);
